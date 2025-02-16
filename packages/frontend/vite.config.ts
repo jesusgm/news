aeb6f350a0131764108158292d216227a4fb3ec3
Jesús Gándara Martínez <jesusalxen@gmail.com>
2025-02-16 09:09:08 +0100

    Change default port

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -4,8 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/news",
   server: {
-    port: 3041,
+    port: 3000,
   },
 });
