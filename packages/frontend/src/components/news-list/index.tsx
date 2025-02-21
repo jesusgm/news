64f3675e47240fc1e2dc1e1d03f963f25bb66f58
Jesús Gándara Martínez <jesusalxen@gmail.com>
2025-02-21 18:33:12 +0100

    Scroll to top when change news

--- packages/frontend/src/components/news-list/index.tsx
+++ packages/frontend/src/components/news-list/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useNews } from "../../hooks/news";
 import { NewCard } from "../new-card";
 
@@ -5,6 +6,10 @@ import styles from "./styles.module.css";
 
 export function NewsList() {
   const { news, loading } = useNews();
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [news]);
+
   return (
     <>
       {loading && <div className={styles.loading}>Loading...</div>}
